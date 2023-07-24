93dc3baa90162d1fc8107d8dd1609a4a0e95c4bb
Takuya Kasagawa <52574278+takuyakasagawa@users.noreply.github.com>
2023-07-24 12:43:02 +0900

    header comit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <header>
-        <div>
-          <a href='#'>
-            <span>Takuya Kasagawa</span>
+      <header className="text-gray-700 border-b border-gray-200">
+        <div className='container flex mx-auto p-5 flex-col md:flex-row items-center '>
+          <a href='#' className='font-medium text-gray-900 mb-4 md:mb-0'>
+            <span className='text-xl ml-1'>Takuya Kasagawa</span>
           </a>
-          <nav></nav>
+          <nav className='md:ml-auto text-base'>
+            <a href='#home' className='mr-5 hover:text-blue-400 duration-300'>Home</a>
+            <a href='#about' className='mr-5 hover:text-blue-400 duration-300'>About</a>
+            <a href='#skills' className='mr-5 hover:text-blue-400 duration-300'>Skills</a>
+            <a href='#blog' className='mr-5 hover:text-blue-400 duration-300'>Blog</a>
+          </nav>
         </div>
       </header>
     </>
