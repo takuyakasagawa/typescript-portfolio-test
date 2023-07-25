f8731a181b80306959e5ee0cb46ad54757c634fe
Takuya Kasagawa <52574278+takuyakasagawa@users.noreply.github.com>
2023-07-25 20:14:45 +0900

    about comit

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
             </p>
             <button className='text-white bg-green-500 py-2 px-6 border-0 rounded text-lg hover:bg-green-600 duration-300'>Contact</button>
           </div>
-          <div className='md:w-1/2 lg:max-w-lg w-1/2' >
+          <div className='md:w-3/2 lg:max-w-lg w-1/2' >
           <img src='./img/icon.jpeg' alt=''/>
           </div>
         </div>
@@ -49,9 +49,9 @@ function App() {
           </div>
           {/* カードのdivタグ */}
           <div className='flex flex-wrap'>
-            <div className='md:w-1/3'>
-              <div className='bg-gray-200 rounded-lg'>
-                <div className='flex items-center'>
+            <div className='md:w-1/3 p-4'>
+              <div className='bg-gray-200 rounded-lg p-8'>
+                <div className='flex items-center mb-3'>
                   <div className='bg-green-500 text-white rounded-full'>
                     <svg
                       width="24" 
@@ -64,7 +64,81 @@ function App() {
                       />
                     </svg>
                   </div>
-                  <h2 className='text-gray-900 text-lg font-medium'>
+                  <h2 className='text-gray-900 text-lg font-medium ml-2'>
+                    Web Developer
+                  </h2>
+                </div>
+                <div>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus vitae excepturi reiciendis corporis expedita unde dolor quia in voluptas quidem.
+                  </p>
+                  <a href='#' className='flex mt-3 text-green-500 items-center'>
+                    もっと見る
+                    <svg 
+                      width="24" 
+                      height="24" 
+                      viewBox="0 0 24 24" 
+                      fill="none" 
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z" fill="currentColor" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className='md:w-1/3 p-4'>
+              <div className='bg-gray-200 rounded-lg p-8'>
+                <div className='flex items-center mb-3'>
+                  <div className='bg-green-500 text-white rounded-full'>
+                    <svg
+                      width="24" 
+                      height="24"
+                      viewBox="0 0 24 24" 
+                      fill="none" 
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z" fill="currentColor" 
+                      />
+                    </svg>
+                  </div>
+                  <h2 className='text-gray-900 text-lg font-medium ml-2'>
+                    Web Developer
+                  </h2>
+                </div>
+                <div>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus vitae excepturi reiciendis corporis expedita unde dolor quia in voluptas quidem.
+                  </p>
+                  <a href='#' className='flex mt-3 text-green-500 items-center'>
+                    もっと見る
+                    <svg 
+                      width="24" 
+                      height="24" 
+                      viewBox="0 0 24 24" 
+                      fill="none" 
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z" fill="currentColor" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className='md:w-1/3 p-4'>
+              <div className='bg-gray-200 rounded-lg p-8'>
+                <div className='flex items-center mb-3'>
+                  <div className='bg-green-500 text-white rounded-full'>
+                    <svg
+                      width="24" 
+                      height="24"
+                      viewBox="0 0 24 24" 
+                      fill="none" 
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z" fill="currentColor" 
+                      />
+                    </svg>
+                  </div>
+                  <h2 className='text-gray-900 text-lg font-medium ml-2'>
                     Web Developer
                   </h2>
                 </div>
